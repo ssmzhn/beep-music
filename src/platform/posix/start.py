b1a42fda43bd8d3119ce546dd9796757bf995921
SSMZHN <ssmzhn@163.com>
2022-02-11 21:31:51 +0800

    正式版114514！

--- src/platform/posix/start.py
+++ src/platform/posix/start.py
@@ -2,5 +2,5 @@ import os
 import sys
 if not os.path.exists('src/platform/posix/a.out'):
     os.system('g++ src/platform/posix/beep.cpp -o src/platform/posix/a.out')
-f=os.popen('./a.out {}'.format(sys.argv[1]))
+f=os.popen('src/platform/posix/a.out {}'.format(sys.argv[1]))
 os.system('src/platform/posix/music.sh '+f.read())
